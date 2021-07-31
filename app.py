9ce5d9b9041fdcca0fa95589aa912ae1b765c83f
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-07-31 18:29:16 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def yurt():
+    word = mongo.db.words.find_one({"delete": "yes"})
+    return render_template("base.html", word=word)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
+
+err_Avoid = env
