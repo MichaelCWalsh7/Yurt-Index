ef1f80e2a475cba0327774871d44fae829e86396
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-08-07 19:29:49 +0000

    Add functionality to the log_in page

--- app.py
+++ app.py
@@ -229,8 +229,30 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/log_in")
+@app.route("/log_in", methods=["GET", "POST"])
 def log_in():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for('home_page'))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("log_in"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("log_in"))
+
     return render_template("log_in.html")
 
 
