f5e7c9af3ce6b22838621cfe6bbbf3711fea019e
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-08-11 08:42:13 +0000

    Fix bug where user profile's weren't loading on log in

--- app.py
+++ app.py
@@ -240,10 +240,11 @@ def log_in():
             # ensure hashed password matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
+                session["user"] = existing_user.get("name")
                 flash("Welcome, {}".format(request.form.get("username")))
                 print("success")
-                return redirect(url_for('home_page'))
+                return redirect(url_for(
+                    'profile', username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -267,9 +268,12 @@ def log_out():
     return redirect(url_for("log_in"))
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/profile/<username>")
+def profile(username):
+    user = mongo.db.users.find_one(
+        {"name": username}
+    )
+    return render_template("profile.html", user=user)
 
 
 if __name__ == "__main__":
