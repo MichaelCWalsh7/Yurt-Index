6efce9dc07c692cb905990c460c44a90c9c31c90
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-08-18 11:46:00 +0000

    Add password/email validation script to register.html only so as to avoid undue console errors

---
+++ static/js/password.js
@@ -0,0 +1,26 @@
+var password = document.getElementById("password"),
+    confirm_password = document.getElementById("confirm_password");
+
+var email = document.getElementById("email"),
+    confirm_email = document.getElementById("confirm_email");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+function validateEmail() {
+    if (email.value != confirm_email.value) {
+        confirm_email.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_email.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+email.onchange = validateEmail;
+confirm_email.onkeyup = validateEmail;
