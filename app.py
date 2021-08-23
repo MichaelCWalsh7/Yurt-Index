72ecb36b8d06b1af3dab7297dba556e9e6d2094e
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-08-23 12:57:54 +0000

    Add & wire up 500 error page

--- app.py
+++ app.py
@@ -676,6 +676,11 @@ def error404(e):
     return render_template("404.html")
 
 
+@app.errorhandler(500)
+def error500(e):
+    return render_template("500.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
